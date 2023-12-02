38485ce7c5e878ade7004f1ad5c3ccaf75b84fba
DzimaSh <dzmitry.shushkevich@gmail.com>
2023-12-02 14:05:41 +0100

    initial commit

---
+++ src/test/java/by/bsu/voicemessages/VoicemessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.voicemessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoicemessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
